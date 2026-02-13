6bb1d47946c82e0cc333501a0f27993da4af2425
Danie1Winn <daniel.winn.biz@gmail.com>
2026-02-12 17:16:28 -0800

    feat(StockBroker.cs): implement provided lab structure with async event handling

--- Stock/StockBroker.cs
+++ Stock/StockBroker.cs
@@ -1,61 +1,75 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
 using System.Text;
+using System.IO;
 using System.Threading;
+
 namespace Stock
 {
-    //!NOTE!: Class StockBroker has fields broker name and a list of Stock named stocks.
-    // addStock method registers the Notify listener with the stock (in addition to
-    // adding it to the lsit of stocks held by the broker). This notify outputs
-    // to the console the name, value, and the number of changes of the stock whose
-    // value is out of the range given the stock's notification threshold.
     public class StockBroker
     {
         public string BrokerName { get; set; }
         public List<Stock> stocks = new List<Stock>();
 
-        //readonly string docPath = @"C:\Users\Documents\CECS 475\Lab3_output.txt";
-        readonly string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lab1_output.txt");
+        readonly string destPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Lab1_Output.txt");
 
         public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) +
             "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
 
-        Console.WriteLine(titles);
-        using (StreamWriter outputFile = new StreamWriter(destPath, false))
-        {
-            outputFile.WriteLine(titles);
-        }   /// <summary>
-            /// The stockbroker object
-            /// </summary>
-            /// <param name="brokerName">The stockbroker's name</param>
         public StockBroker(string brokerName)
         {
             BrokerName = brokerName;
+            // Print the header to console
+            Console.WriteLine(titles);
+
+            // Overwrite (false) the file with this same header once
+            using (StreamWriter outputFile = new StreamWriter(destPath, false))
+            {
+                outputFile.WriteLine(titles);
+            }
         }
-        //------------------------------------------------------------------------------------------------------------------
-        /// <summary>
-        /// Adds stock objects to the stock list
-        /// </summary>
-        /// <param name="stock">Stock object</param>
+
         public void AddStock(Stock stock)
         {
             stocks.Add(stock);
             stock.StockEvent += EventHandler;
         }
-        /// <summary>
-        /// The eventhandler that raises the event of a change
-        /// </summary>
-        /// <param name="sender">The sender that indicated a change</param>
-        /// <param name="e">Event arguments</param>
-        public async void EventHandler(Object sender, StockNotification e)
+
+        private async void EventHandler(object sender, EventArgs e)
         {
-            Stock newStock = (Stock)sender;
-            await write(sender, e);
-            return;
+            if (sender is not null)
+                // The second parameter needs to be cast to StockNotification
+                await Helper(sender, (StockNotification) e);
         }
+
+        public async Task Helper(object sender, StockNotification e)
+        {
+            // We could cast the sender back to Stock if we needed more info
+            Stock newStock = (Stock)sender;
+            // Construct the output line
+            string message =
+            $"{BrokerName.PadRight(10)}" +
+            $"{e.StockName.PadRight(15)}" +
+            $"{e.CurrentValue.ToString().PadRight(10)}" +
+            $"{e.NumChanges.ToString().PadRight(10)}" +
+            $"{DateTime.Now}";
+            try
+            {
+                // Append this line to the output file
+                using (StreamWriter outputFile = new StreamWriter(destPath, true))
+                {
+                    await outputFile.WriteLineAsync(message);
+                }
+                // Also write to console
+                Console.WriteLine(message);
+            }
+            catch (IOException ex)
+            {
+                // Handle or log any file I/O exceptions if needed
+            }
+        }
+
+        /*
         public async Task write(Object sender, StockNotification e)
         {
             String line = BrokerName.PadRight(16) + e.StockName.PadRight(16) +
@@ -82,5 +96,6 @@ namespace Stock
                 Console.WriteLine($"Error writing to file: {ex.Message}");
             }
         }
+        */
     }
 }
